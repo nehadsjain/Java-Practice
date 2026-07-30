a9ae04830c07ccc0db1dab10b933b769ff97091f
Neha <nehadsjain1@gmail.com>
2026-07-30 22:15:14 +0530

    Added even odd problem

---
+++ src/com/pack/EvenOdd.java
@@ -0,0 +1,16 @@
+package com.pack;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = s.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
