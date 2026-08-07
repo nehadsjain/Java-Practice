34b206efd12096ae7ad960fee6f0be0861d30751
Neha <nehadsjain1@gmail.com>
2026-08-07 19:30:20 +0530

    Added basic program

---
+++ src/DoWhile/Numbers.java
@@ -0,0 +1,15 @@
+package DoWhile;
+
+import java.util.Scanner;
+public class Numbers {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n = s.nextInt();
+        int i=1;
+        do{
+            System.out.print(i+"\t");
+            i++;
+        }while (i<=10);
+    }
+}
