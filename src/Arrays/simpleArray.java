909c1a3f1a1be6282f8a57203f74cb85ded59bb9
Neha <nehadsjain1@gmail.com>
2026-08-08 16:26:33 +0530

    Simple Array program

--- src/Arrays/simpleArray.java
+++ src/Arrays/simpleArray.java
@@ -8,10 +8,10 @@ public class simpleArray {
         int n =s.nextInt();
         int[] a = new int[n];
         System.out.println("Enter elements:");
-        for (int i=0;i<n;i++){
+        for (int i=0;i<a.length;i++){
             a[i] = s.nextInt();
         }
-        for (int i=0;i<n;i++){
+        for (int i=0;i<a.length;i++){
             System.out.print(a[i]+"\t");
         }
     }
