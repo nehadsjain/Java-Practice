9672ffe0da893e0c7ed52387f34e5f25e5965899
Neha <nehadsjain1@gmail.com>
2026-08-08 16:39:21 +0530

    Array Sum  program

---
+++ src/Arrays/ArraySum.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class ArraySum {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter no.of elements:");
+        int n =s.nextInt();
+        int[] a = new int[n];
+        int sum=0;
+        System.out.println("Enter elements:");
+        for (int i=0;i<a.length;i++){
+            a[i] = s.nextInt();
+        }
+        for (int i=0;i<a.length;i++){
+            sum += a[i];
+        }
+        System.out.println("Sum:"+sum);
+    }
+}
