83f71de8dbd85b9a026dabc308cb3ecc293e276a
Neha <nehadsjain1@gmail.com>
2026-08-14 18:46:28 +0530

    Rotate array by 1 position

---
+++ src/Arrays/RotateArray.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class RotateArray {
+    static void rotate(int[] a){
+        int last = a[a.length-1];
+        for (int i=a.length-1;i>=1;i--){
+            a[i]=a[i-1];
+        }
+        a[0]=last;
+        for (int i=0;i<a.length;i++){
+            System.out.print(a[i]+"\t");
+        }
+    }
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter no.of elements:");
+        int n =s.nextInt();
+        int[] a = new int[n];
+        System.out.println("Enter elements:");
+        for (int i=0;i<a.length;i++){
+            a[i] = s.nextInt();
+        }
+        for (int i=0;i<a.length;i++){
+            System.out.println(a[i]+"\t");
+        }
+        rotate(a);
+    }
+}
