c53f63cd2c8e547b98e187198b89848149b7139d
Neha <nehadsjain1@gmail.com>
2026-08-19 16:27:30 +0530

    Removing duplicates from the String

--- src/String/RemoveDuplicate.java
+++ src/String/RemoveDuplicate.java
@@ -1,4 +1,25 @@
 package String;
 
+import java.util.Scanner;
+
 public class RemoveDuplicate {
+    static void duplicate(String str){
+        String str1 = "";
+        for (int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if (str1.contains(String.valueOf(ch))){
+                continue;
+            }else {
+                str1 = str1 + ch;
+            }
+        }
+        System.out.println(str1);
+    }
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a string:");
+        String str = s.nextLine();
+        System.out.println("String:"+str);
+        duplicate(str);
+    }
 }
