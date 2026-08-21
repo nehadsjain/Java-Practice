312524687e980b5c8654651ed09bafc9e06f4905
Neha <nehadsjain1@gmail.com>
2026-08-21 19:32:25 +0530

    Reverse string using StringBuilder

--- src/String/ReverseStringBuilder.java
+++ src/String/ReverseStringBuilder.java
@@ -1,4 +1,16 @@
 package String;
 
+import java.util.Scanner;
+
 public class ReverseStringBuilder {
+    static void reverseUsingBuilder(String str){
+        StringBuilder vnam = new StringBuilder(str);
+        System.out.println("String:"+vnam);
+        System.out.println("Reverse:"+vnam.reverse());
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        reverseUsingBuilder(str);
+    }
 }
