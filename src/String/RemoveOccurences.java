e9e650e4d89478a2f08e99240908110695d9e7f9
Neha <nehadsjain1@gmail.com>
2026-09-18 22:32:54 +0530

    Remove all occurences of a given character from a string

--- src/String/RemoveOccurences.java
+++ src/String/RemoveOccurences.java
@@ -1,4 +1,25 @@
 package String;
 
+import java.util.Scanner;
+
 public class RemoveOccurences {
+    static void removeOccurenceString(String str,char ch){
+        StringBuilder res = new StringBuilder();
+        for (int i=0;i<str.length();i++){
+            if (str.charAt(i) == ch){
+                continue;
+            }else {
+                res.append(str.charAt(i));
+            }
+        }
+        System.out.println(res);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string:");
+        String str = sc.nextLine();
+        System.out.print("Enter the Character to be removed:");
+        char ch = sc.next().charAt(0);
+        removeOccurenceString(str,ch);
+    }
 }
